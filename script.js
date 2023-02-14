1bc9db821c4754a7562c31949a92c2e3510a311c
Nnachi Helen Chidinma <105408064+helzuby@users.noreply.github.com>
2023-02-14 19:34:14 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,11 @@
+//Write a javascript function to set the background color of that paragraph
+// using an "onclick" event added to a button in the UI.
+//steps to follow
+// Create a HTML page with a p element
+// Create a button tag with onclick and a javascriptfunction.
+// write a javascript function and set the desired background color.
+
+
+function myFunction() {
+  document.getElementById('same').style.backgroundColor = "blue";
+}
